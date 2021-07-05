f42c0b54104e9bf289dbc43ce50607b0aba9f047
Michel dos Santos <80466717+MichellSantos@users.noreply.github.com>
2021-07-05 14:26:20 -0300

    ensure RemoteAddAccount completes with error when HttpClient completes with error

---
+++ CleanApp/Data/Http/HttpError.swift
@@ -0,0 +1,12 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by Michel dos Santos on 05/07/21.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+    case noConnectivity
+}
---
+++ CleanApp/Domain/Error/DomainError.swift
@@ -0,0 +1,12 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by Michel dos Santos on 02/07/21.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected
+}
