5a0327c1794ef9fdf51b571d2bc20ae72c7f3a2a
Michel dos Santos <80466717+MichellSantos@users.noreply.github.com>
2021-07-05 14:49:14 -0300

    change AddAccount and HttpPostClient to use Result instead  of error

--- CleanApp/DataTests/UseCases/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/UseCases/RemoteAddAccountTests.swift
@@ -27,8 +27,11 @@ class RemoteAddAccountTests: XCTestCase {
     func test_add_should_complete_httpClient_with_error_if_client_fails() {
         let (sut, httpClientSpy) = makeSut()
         let exp = expectation(description: "waiting")
-        sut.add(addAccountModel: makeAddAccountModel()) { error in
-            XCTAssertEqual(error, .unexpected)
+        sut.add(addAccountModel: makeAddAccountModel()) { result in
+            switch result {
+            case .failure(let error): XCTAssertEqual(error, .unexpected)
+            case .success(_): XCTFail("Expected error receive \(result) instead")
+            }
             exp.fulfill()
         }
         httpClientSpy.completeWithError(.noConnectivity)
@@ -49,16 +52,16 @@ extension RemoteAddAccountTests {
     class HttpClientSpy: HttpPostClient {
         var urls = [URL]()
         var data: Data?
-        var completion: ((HttpError) -> Void)?
+        var completion: ((Result<Data,HttpError>) -> Void)?
         
-        func post(to url: URL, with data: Data?, completion: @escaping (HttpError) -> Void) {
+        func post(to url: URL, with data: Data?, completion: @escaping (Result<Data,HttpError>) -> Void) {
             self.urls.append(url)
             self.data = data
             self.completion = completion
         }
         
         func completeWithError(_ error: HttpError) {
-            completion?(error)
+            completion?(.failure(error))
         }
     }
 }
